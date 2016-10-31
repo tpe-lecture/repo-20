b2c238579b9abd6fda44c16ee0acaf1081d53621
r.lubaschewski <r.lubaschewski@bw-lp>
2016-10-31 10:59:04 +0100

    abstrakte Klasse eingefügt, Geometer verändert

---
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Figur.java
@@ -0,0 +1,16 @@
+package tpe.oo.abstrakt;
+/**
+ * eine abstrakte Figur mit einer Fläche.
+ * @author r.lubaschewski
+ *
+ */
+public abstract class Figur {
+   /**
+    *
+    * @return gibt die Fläche der Figur zurück
+    */
+    protected abstract double flaeche();
+
+
+
+}
