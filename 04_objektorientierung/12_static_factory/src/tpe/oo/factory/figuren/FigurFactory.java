274cb597c02d8441c60d7d51c34e759987a23269
r.lubaschewski <r.lubaschewski@bw-lp>
2016-10-31 16:01:15 +0100

    aufgabe angefangen

---
+++ 04_objektorientierung/12_static_factory/src/tpe/oo/factory/figuren/FigurFactory.java
@@ -0,0 +1,14 @@
+package tpe.oo.factory.figuren;
+
+import java.util.Random;
+
+public class FigurFactory {
+
+    Random bla=new Random();
+
+    public static Figur createFigur(){
+        return
+    }
+
+
+}
